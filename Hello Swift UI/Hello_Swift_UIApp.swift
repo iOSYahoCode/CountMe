12813fbfd0bb8f18c8c73a3009bdb69c05148d75
Yaroslav Homziak <yaroslav.homziak@adv.ua>
2025-05-13 09:13:48 +0300

    Initial Commit

---
+++ Hello Swift UI/Hello_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_UIApp.swift
+//  Hello Swift UI
+//
+//  Created by Yaroslaw Homziak on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
