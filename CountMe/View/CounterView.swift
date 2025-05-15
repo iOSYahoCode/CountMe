c85fb050a63080b1ee041b363e6204d1436d6f1a
YahoCode <yahocode@gmail.com>
2025-05-15 08:44:50 +0300

    feat (button and animation) : add reset value button. Animated show and hide procces for this button when countev value is > 0

--- CountMe/View/CounterView.swift
+++ CountMe/View/CounterView.swift
@@ -26,16 +26,35 @@ struct CounterView: View {
             
             Spacer()
             
-            Button(action: {
-                viewModel.increment()
-            }) {
-                Text("Add one")
-                    .frame(width: 200, height: 50)
-                    .foregroundStyle(.white)
-                    .background(colorScheme == .dark ? .gray : .black)
-                    .clipShape(.buttonBorder)
-                    .padding(.bottom)
+            HStack{
+                Button(action: {
+                    viewModel.increment()
+                }) {
+                    Text("Add one")
+                        .frame(width: 200, height: 50)
+                        .foregroundStyle(.white)
+                        .background(colorScheme == .dark ? .gray : .black)
+                        .clipShape(.buttonBorder)
+                }
+                if viewModel.score > 0 {
+                    Button {
+                        withAnimation {
+                            viewModel.reset()
+                        }
+                    } label: {
+                        Image(systemName: "trash.fill")
+                            .frame(width: 35, height: 50)
+                            .foregroundStyle(.white)
+                            .background(colorScheme == .dark ? .gray : .black)
+                            .clipShape(.buttonBorder)
+                    }
+                    .transition(.move(edge: .trailing).combined(with: .opacity))
+                }
+                
             }
+            .padding(.bottom)
+            .animation(.easeInOut(duration: 0.3), value: viewModel.score)
+            
             
         }
         .onAppear(){
